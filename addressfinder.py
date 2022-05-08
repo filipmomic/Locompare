ef7b6d0cdfd24b923eddaf16e254478f0a6d73ea
Filip <71992827+filipmomic@users.noreply.github.com>
2022-05-07 20:35:48 -0500

    Clean up global variable

--- addressfinder.py
+++ addressfinder.py
@@ -3,9 +3,6 @@ import streamlit as st
 
 @st.cache(hash_funcs={"_thread.RLock": lambda _: None, "builtins.weakref": lambda _: None })
 def getInputAddress(inputAddress):
-    noValidAddress = True
-    global location
-    location = ""
     
     # find lat/long of entered addresses
     locator = Nominatim(user_agent=st.secrets["user_agent"])
